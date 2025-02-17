1554d6945a970c7050472c4f3293fa8c23fd6a41
jasonbarrow <barrow@jasonbarrow.com>
2025-02-17 15:24:02 -0500

    Solve 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (initialText, numberOfTimes) {
+  if (numberOfTimes < 0) {
+    return "ERROR";
+  }
+  let finalText = "";
+  for (let i = 0; i < numberOfTimes; i++) {
+    finalText += initialText;
+  }
+  return finalText;
 };
 
 // Do not edit below this line
