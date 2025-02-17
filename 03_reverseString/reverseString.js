ac8bd2cbd91ace9cadd2db2fc83a928fe817d4fc
jasonbarrow <barrow@jasonbarrow.com>
2025-02-17 16:08:06 -0500

    Solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (inputText) {
+  let initialArray = inputText.split("");
+  let reversedArray = initialArray.reverse();
+  let reversedString = reversedArray.join("");
+  return reversedString;
 };
 
 // Do not edit below this line
