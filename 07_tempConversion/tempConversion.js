afd788482d46b67ab3d973c9daf3726fa18e2b5c
jasonbarrow <barrow@jasonbarrow.com>
2025-02-18 15:31:20 -0500

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahnrenheitNumber) {
+  let celsiusNumber = 0;
+  celsiusNumber = (fahnrenheitNumber - 32) / (9 / 5);
+  if (!Number.isInteger(celsiusNumber)) {
+    celsiusNumber = Number(celsiusNumber.toFixed(1));
+    return celsiusNumber;
+  } else {
+    return celsiusNumber;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusNumber) {
+  let fahnrenheitNumber = 0;
+  fahnrenheitNumber = (celsiusNumber * 9) / 5 + 32;
+  if (!Number.isInteger(fahnrenheitNumber)) {
+    fahnrenheitNumber = Number(fahnrenheitNumber.toFixed(1));
+    return fahnrenheitNumber;
+  } else {
+    return fahnrenheitNumber;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
