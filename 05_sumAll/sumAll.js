41723e2527704fd4c3ad31e57aaa0296ddbde939
jasonbarrow <barrow@jasonbarrow.com>
2025-02-18 14:50:19 -0500

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (startNumber, endNumber) {
+  let sumResult = 0;
+  if (startNumber < 0 || endNumber < 0) {
+    return "ERROR";
+  }
+  if (!Number.isInteger(startNumber) || !Number.isInteger(endNumber)) {
+    return "ERROR";
+  }
+  if (startNumber < endNumber) {
+    for (let i = startNumber; i <= endNumber; i++) {
+      sumResult += i;
+    }
+  } else {
+    for (let i = endNumber; i <= startNumber; i++) {
+      sumResult += i;
+    }
+  }
+  return sumResult;
 };
 
 // Do not edit below this line
