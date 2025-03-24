17aacc7498e69c61f91ad3716eda4b6cde34565b
jasonbarrow <barrow@jasonbarrow.com>
2025-03-24 14:47:17 -0400

    Solve 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (inputArray) {
+  // return inputArray.reduce((summy, current) => summy + current, 0);
+
+  let value = inputArray.reduce(function (summy, current) {
+    return summy + current;
+  }, 0);
+  return value;
 };
 
-const multiply = function() {
-
+const multiply = function (inputArray) {
+  let value = inputArray.reduce(function (multi, current) {
+    return multi * current;
+  });
+  return value;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0) {
+    return 1;
+  } else {
+    let result = 1;
+    for (i = a; i > 0; i--) {
+      result = result * i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
