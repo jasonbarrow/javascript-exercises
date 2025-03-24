2a103a22d98cab40e4243736fbdde4670def4a9a
jasonbarrow <barrow@jasonbarrow.com>
2025-03-24 15:47:27 -0400

    Solve 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+  let lowerCaseString = inputString.toLowerCase();
+  const lowerCaseArray = Array.from(lowerCaseString);
 
+  const filteredArray = lowerCaseArray.filter((char) =>
+    /^[a-z0-9]$/.test(char),
+  );
+  const filteredString = filteredArray.join("");
+
+  const reversedArray = filteredArray.reverse();
+  const reversedString = reversedArray.join("");
+
+  if (filteredString === reversedString) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
