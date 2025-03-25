a0eedb668a6615f3cc57d8eda784b5bf666a0e9d
jasonbarrow <barrow@jasonbarrow.com>
2025-03-25 15:33:44 -0400

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (numInSeries) {
+  numInSeries = parseInt(numInSeries);
+  if (numInSeries === 0) {
+    return 0;
+  } else if (numInSeries < 1) {
+    return "OOPS";
+  } else if (numInSeries < 3) {
+    return 1;
+  } else {
+    let numbers = [1, 1];
+    for (let i = 1; i < numInSeries; i++) {
+      numbers.push(numbers[i] + numbers[i - 1]);
+    }
+    console.log(numbers);
+    return numbers[numInSeries - 1];
+  }
 };
 
 // Do not edit below this line
