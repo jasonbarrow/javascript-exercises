d3c1832a5bb80d99977f7963de62587e2d4f60e4
jasonbarrow <barrow@jasonbarrow.com>
2025-03-25 16:10:11 -0400

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (inputArray) {
+  const date = new Date();
+  const year = date.getFullYear();
 
+  for (let i = 0; i < inputArray.length; i++) {
+    if (!inputArray[i].hasOwnProperty("yearOfDeath")) {
+      inputArray[i]["yearOfDeath"] = year;
+    }
+  }
+
+  const sortedArray = inputArray.sort(function (a, b) {
+    if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  return sortedArray[0];
 };
 
 // Do not edit below this line
